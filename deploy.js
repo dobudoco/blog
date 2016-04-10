43b21c036eb8e98b490bc48febd0c7474785e4b4
Lucas <lucas@agtlucas.com>
2016-04-10 15:26:45 -0300

    Update deploy

--- deploy.js
+++ deploy.js
@@ -2,5 +2,9 @@ var gh = require('gh-pages')
 var path = require('path')
 
 gh.publish(path.join(__dirname, 'public'), function (err) {
-  console.log(err)
+  if (err) {
+    console.log(err)
+  } else {
+    console.log('Hee hoo!')
+  }
 })
