479843c8994cd41d3990180233aa2674d690a5aa
Lucas <lucas@agtlucas.com>
2016-04-14 20:52:41 -0300

    Update gulpfile

--- themes/budo/gulpfile.js
+++ themes/budo/gulpfile.js
@@ -1,6 +1,18 @@
 'use strict'
 
 const gulp = require('gulp')
-const postcss = require('gulp-postcss')
 const stylus = require('gulp-stylus')
+const cssnano = require('cssnano')
+const postcss = require('gulp-postcss')
 const autoprefixer = require('autoprefixer')
+
+gulp.task('css', () => {
+  var processors = [
+    stylus(),
+    autoprefixer({ browsers: ['last 1 version'] }),
+    cssnano()
+  ]
+  return gulp.src('source/stylus/*.styl')
+    .pipe(postcss(processors))
+    .pipe(gulp.dest('build/css'))
+})
